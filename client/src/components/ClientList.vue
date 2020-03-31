f59e7caa43653c5b9f055cbbcc48958e4c73f092
Gscolera <goroshnikovanton@gmail.com>
2020-03-31 15:20:25 +0300

    minor fix

--- client/src/components/ClientList.vue
+++ client/src/components/ClientList.vue
@@ -1,43 +1,5 @@
 <template>
   <b-container class="pt-md-5 px-0">
-    <b-navbar toggleable type="dark" class="bg-secondary justify-content-space-between">
-      <b-navbar-brand>Clients</b-navbar-brand>
-      <span class="d-flex">
-        <b-navbar-toggle target="settings">
-          <template v-slot:default="{ expanded }">
-            <b-icon-chevron-bar-up v-if="expanded" />
-            <b-icon-chevron-bar-down v-else />
-          </template>
-        </b-navbar-toggle>
-        <b-btn size="sm" class="ml-2" @click="$bvModal.show('form')">
-          <b-icon-person-plus-fill />
-        </b-btn>
-      </span>
-      <b-collapse id="settings" is-nav>
-        <b-container class="px-0 py-3">
-          <b-row class="w-100 mb-2">
-            <b-col cols="12" md="6" class="mb-2 mb-md-0">
-              <b-form-select v-model="sort" :options="sortOptions" class="bg-secondary text-white" size="sm" />
-            </b-col>
-            <b-col cols="12" md="6">
-              <b-form-select v-model="order" class="bg-secondary text-white" size="sm">
-                <b-form-select-option value="null">---Sort order---</b-form-select-option>
-                <b-form-select-option value="asc">ascending</b-form-select-option>
-                <b-form-select-option value="desc">descending</b-form-select-option>
-              </b-form-select>
-            </b-col>
-          </b-row>
-          <b-row class="w-100">
-            <b-col cols="12" md="6" class="mb-2 mb-md-0">
-              <b-form-select v-model="filterBy" :options="filterOptions" class="bg-secondary text-white" size="sm" />
-            </b-col>
-            <b-col cols="12" md="6">
-              <b-form-input v-model="filter" @input="filterCLients" size="sm"/>
-            </b-col>
-          </b-row>
-        </b-container>
-      </b-collapse>
-    </b-navbar>
     <b-table
       :fields="fields"
       :items="clientsList"
@@ -45,15 +7,65 @@
       stacked="md"
       outlined
       id="client-list"
+      class="w-100"
+      caption-top
     >
+      <template v-slot:table-caption class="sdf">
+        <b-navbar toggleable type="dark" class="my-0 bg-secondary justify-content-space-between">
+          <b-navbar-brand>Clients</b-navbar-brand>
+          <span class="d-flex">
+            <b-navbar-toggle target="settings">
+              <template v-slot:default="{ expanded }">
+                <b-icon-chevron-bar-up v-if="expanded" />
+                <b-icon-chevron-bar-down v-else />
+              </template>
+            </b-navbar-toggle>
+            <b-btn size="sm" class="ml-2" @click="$bvModal.show('form')">
+              <b-icon-person-plus-fill />
+            </b-btn>
+          </span>
+          <b-collapse id="settings" is-nav>
+            <b-container class="px-0 py-3">
+              <b-row class="w-100 mb-2">
+                <b-col cols="12" md="6" class="mb-2 mb-md-0">
+                  <b-form-select v-model="sort" :options="sortOptions" class="bg-secondary text-white" size="sm" />
+                </b-col>
+                <b-col cols="12" md="6">
+                  <b-form-select v-model="order" class="bg-secondary text-white" size="sm">
+                    <b-form-select-option value="null">---Sort order---</b-form-select-option>
+                    <b-form-select-option value="asc">ascending</b-form-select-option>
+                    <b-form-select-option value="desc">descending</b-form-select-option>
+                  </b-form-select>
+                </b-col>
+              </b-row>
+              <b-row class="w-100">
+                <b-col cols="12" md="6" class="mb-2 mb-md-0">
+                  <b-form-select 
+                    v-model="filterBy" 
+                    :options="filterOptions" 
+                    @change="filterCLients" 
+                    class="bg-secondary text-white" 
+                    size="sm" 
+                  />
+                </b-col>
+                <b-col cols="12" md="6">
+                  <b-form-input v-model="filter" @input="filterCLients" size="sm"/>
+                </b-col>
+              </b-row>
+            </b-container>
+          </b-collapse>
+        </b-navbar>
+      </template>
       <template v-slot:cell(name)="data">{{ data.item.name }}</template>
       <template v-slot:cell(email)="data">{{ data.item.email }}</template>
       <template v-slot:cell(phone)="data">{{ data.item.phone }}</template>
       <template v-slot:cell(providers)="data">
+        <b-container class="d-flex flex-wrap">
         <span v-for="(provider, i) in data.item.providers" :key="provider.id">
           <span>{{ provider.name }}</span>
           <span v-if="i < data.item.providers.length - 1" class="mr-1">,</span>
         </span>
+        </b-container>
       </template>
       <template v-slot:cell(actions)="data">
         <b-button-group size="sm">
